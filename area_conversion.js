4e3d84aecb11de7fa2cd0a035ee4283cd466d79c
jfnaro <35393484+jfnaro@users.noreply.github.com>
2019-10-21 14:10:24 -0400

    Visualizer implemented
    Visualizer implemented

--- area_conversion.js
+++ area_conversion.js
@@ -10,31 +10,31 @@ function areaConverter(input, primaryUnit, secondaryUnit){
       return input
     }
     switch(primaryUnit){
-      case "mm":
+      case "mm^2":
         value = input * 0.000001
         break;
-      case "cm":
+      case "cm^2":
         value = input * 0.0001;
         break;
-      case "m":
+      case "m^2":
         value = input;
         break;
-      case "km":
+      case "km^2":
         value = input * 1000000;
         break;
       case "ha":
         value = input * 10000;
         break;
-      case "in":
+      case "in^2":
         value = input * 0.00064516;
         break;
-      case "ft":
+      case "ft^2":
         value = input * 0.09290304;
         break;
-      case "yd":
+      case "yd^2":
         value = input * 0.83612736;
         break;
-      case "mi":
+      case "mi^2":
         value = input * 2589988.110336;
         break;
       case "ac":
@@ -42,23 +42,23 @@ function areaConverter(input, primaryUnit, secondaryUnit){
         break;
     }
     switch(secondaryUnit){
-      case "mm":
+      case "mm^2":
         return value * 1000000;
-      case "cm":
+      case "cm^2":
         return value * 10000;
-      case "m":
+      case "m^2":
         return value;
-      case "km":
+      case "km^2":
         return value * 0.0000010;
       case "ha":
         return value * 0.0001;
-      case "in":
+      case "in^2":
         return value * 1550.0031000062;
-      case "ft":
+      case "ft^2":
         return value * 10.76391041671;
-      case "yd":
+      case "yd^2":
         return value * 1.1959900463011;
-      case "mi":
+      case "mi^2":
         return value * 0.00000038610215854245;
       case "ac":
         return value * 0.00024710538146717;
