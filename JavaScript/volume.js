1f8edb1a6b7b305a2d196d70e82c1e8b50a5bb34
Damon Lepine <damonlep@buffalo.edu>
2019-10-30 12:23:42 -0400

    added all existing js files to js folder and anchored  them to appropriate html files

--- JavaScript/volume.js
+++ JavaScript/volume.js
@@ -703,14 +703,4 @@ function generateVisual(){
   ctx.fillText(document.getElementById("secondaryInput").value, canvas.width/2, canvas.height-30);
 }
 
-//Swaps units
-function switchUnits(){
-  var holdSelect = document.getElementById("primarySelect").options[document.getElementById("primarySelect").selectedIndex].value;
-  var holdInput = document.getElementById("primaryInput").value;
-  document.getElementById("primarySelect").value = document.getElementById("secondarySelect").options[document.getElementById("secondarySelect").selectedIndex].value;
-  document.getElementById("primaryInput").value = document.getElementById("secondaryInput").value;
-  document.getElementById("secondarySelect").value = holdSelect;
-  document.getElementById("secondaryInput").value = holdInput;
-}
-
 //File ends here
