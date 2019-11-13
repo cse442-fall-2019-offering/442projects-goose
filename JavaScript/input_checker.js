3cdf1157f9698210b6e056b2a264e48127954dd6
EdwinChiu <edwinchiu123@gmail.com>
2019-11-12 22:56:55 -0500

    Converter no longer accepts impossible values (temperatures below absolute zero and negative lengths, area, volume, time, and speed)

--- JavaScript/input_checker.js
+++ JavaScript/input_checker.js
@@ -1,7 +1,21 @@
 function checkInput(){
     var input = document.getElementById('primaryInput').value;
+    console.log(input);
     if (input != '') {
+      if (document.getElementById('primarySelect').value == "°F" || document.getElementById('primarySelect').value == "°C"){
+          if (calculateConversion(input, document.getElementById("primarySelect").value, "K") < 0){
+            document.getElementById('convertButton').disabled = true;
+          }
+          else{
+            document.getElementById('convertButton').disabled = false;
+          }
+      }
+      else if (input < 0){
+        document.getElementById('convertButton').disabled = true;
+      }
+      else{
         document.getElementById('convertButton').disabled = false;
+      }
     }
     else {
         document.getElementById('convertButton').disabled = true;
