9529e0aff2a19cb6a534b730a11fc91f24b95848
EdwinChiu <edwinchiu123@gmail.com>
2019-12-03 18:37:47 -0500

    fixed unit_switcher so it swaps values stored from converting

--- JavaScript/unit_switcher.js
+++ JavaScript/unit_switcher.js
@@ -1,8 +1,10 @@
 function switchUnits(){
     var holdSelect = document.getElementById("primarySelect").options[document.getElementById("primarySelect").selectedIndex].value;
-    var holdInput = document.getElementById("primaryInput").value;
+    var holdInput = input;
+    input = output;
+    output = holdInput;
     document.getElementById("primarySelect").value = document.getElementById("secondarySelect").options[document.getElementById("secondarySelect").selectedIndex].value;
-    document.getElementById("primaryInput").value = document.getElementById("secondaryInput").value;
+    document.getElementById("primaryInput").value = input;
     document.getElementById("secondarySelect").value = holdSelect;
-    document.getElementById("secondaryInput").value = holdInput;
+    document.getElementById("secondaryInput").value = output;
   }
