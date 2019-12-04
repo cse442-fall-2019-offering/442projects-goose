cbbc3c10d5120003f2a6f783311953f6d0fa3c27
EdwinChiu <edwinchiu123@gmail.com>
2019-12-04 01:36:48 -0500

    scrolling now checks input so the convert button is enabled if inputs are valid

--- JavaScript/scrollable.js
+++ JavaScript/scrollable.js
@@ -2,22 +2,26 @@
 
 function scrollDown(){
   document.getElementById("primaryInput").value = document.getElementById("downTo").value;
+  checkInput();
   convertUnits();
   generateVisual();
 }
 function scrollUp(){
   document.getElementById("primaryInput").value = document.getElementById("upTo").value;
+  checkInput();
   convertUnits();
   generateVisual();
 }
 
 function scrollDownTemp(){
   document.getElementById("primaryInput").value = document.getElementById("downTo").value;
+  checkInput();
   convertUnits();
   generateVisualTemp();
 }
 function scrollUpTemp(){
   document.getElementById("primaryInput").value = document.getElementById("upTo").value;
+  checkInput();
   convertUnits();
   generateVisualTemp();
 }
